7dc5f3322bfc67b2ebade1f5c93bc5b25db7b0dc
Nguyen Tien Tung <ntt091103@gmail.com>
2024-07-02 00:05:22 +0700

    Thêm Entity Comic

---
+++ src/main/java/com/dt/comicWebsite/models/Comic.java
@@ -0,0 +1,122 @@
+package com.dt.comicWebsite.models;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comics")
+public class Comic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+    // field
+    private String name;
+    private String Author;
+    private String status;
+    private String country;
+    private int likes;
+    private int views;
+    private int subscribes;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public int getSubscribes() {
+        return subscribes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public void setSubscribes(int subscribes) {
+        this.subscribes = subscribes;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
