4f04db0785faac08e443dde433b9114da9bc944b
StavidPhan <thanhduyphan2123@gmail.com>
2024-07-01 10:44:59 +0700

    init projects

---
+++ src/test/java/com/dt/comicWebsite/ComicWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.comicWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
