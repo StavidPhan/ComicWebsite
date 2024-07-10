4023c4206eb7a66e513fc42f0e3a1e50f0818cb8
StavidPhan <thanhduyphan2123@gmail.com>
2024-07-10 15:08:19 +0700

    resolve conflicts

--- src/main/java/com/dt/comicWebsite/controllers/admin/ComicController.java
+++ src/main/java/com/dt/comicWebsite/controllers/admin/ComicController.java
@@ -1,16 +1,35 @@
 package com.dt.comicWebsite.controllers.admin;
 
-
+import com.dt.comicWebsite.models.Comic;
+import com.dt.comicWebsite.servies.ComicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("admin/comic")
+@RequestMapping("/admin/comics")
 public class ComicController {
-
-    @GetMapping
-    public String index () {
+    @Autowired
+    private ComicService comicService;
+    @GetMapping({"", "/"})
+    public String showComicList(Model model) {
+        List<Comic> comics = comicService.getAll();
+        model.addAttribute("comics", comics);
         return "admin/comic/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("comic", new Comic());
+        return "admin/comic/createComic";
+    }
+
+    @PostMapping("/create")
+    public String createComic(@ModelAttribute Comic comic) {
+        comicService.create(comic);
+        return "redirect:/admin/comics";
     }
 }
