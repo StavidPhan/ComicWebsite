4d56cb4fe3ec278920563cbd602735d186dfb2d1
Nguyen Tien Tung <ntt091103@gmail.com>
2024-07-29 22:50:54 +0700

    Feat: CRUD Chapter

--- src/main/java/com/dt/comicWebsite/repositories/ChapterRepository.java
+++ src/main/java/com/dt/comicWebsite/repositories/ChapterRepository.java
@@ -3,5 +3,5 @@ package com.dt.comicWebsite.repositories;
 import com.dt.comicWebsite.models.Chapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChapterRepository extends JpaRepository <Chapter, Integer> {
+public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
 }
