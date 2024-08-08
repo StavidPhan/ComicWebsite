1688b042a088a65ad1ab0d553cc31e583c39e49a
StavidPhan <thanhduyphan2123@gmail.com>
2024-08-08 17:58:19 +0700

    validate

--- src/main/java/com/dt/comicWebsite/controllers/admin/CategoryController.java
+++ src/main/java/com/dt/comicWebsite/controllers/admin/CategoryController.java
@@ -58,6 +58,11 @@ public class CategoryController {
 
     @PostMapping("/edit")
     public String editCategory(@RequestParam int id, @Valid @ModelAttribute Category category, BindingResult bindingResult) {
+        // check if name is already exists
+        if (categoryService.findByName(category.getName()) != null) {
+            bindingResult.rejectValue("name", "error.category", "Category name is already exists.");
+        }
+
         if (bindingResult.hasErrors()) {
             return "admin/category/editCategory";
         }
--- src/main/java/com/dt/comicWebsite/repositories/ChapterRepository.java
+++ src/main/java/com/dt/comicWebsite/repositories/ChapterRepository.java
@@ -1,7 +1,9 @@
 package com.dt.comicWebsite.repositories;
 
 import com.dt.comicWebsite.models.Chapter;
+import com.dt.comicWebsite.models.Comic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
+    Chapter findByComicAndChapterNumber(Comic comic, int chapterNumber);
 }
